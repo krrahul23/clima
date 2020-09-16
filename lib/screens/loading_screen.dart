0e02edeea3e089cb189ac99ba102ba0e4683e1b1
krrahul23 <raulk023@gmail.com>
2020-09-16 08:22:48 +0530

    Updated State

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -19,7 +19,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
